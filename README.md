629af58bf122c11be9547a9e63687fc58b21fea7
giovannids <gioadsanchez@gmail.com>
2021-06-28 18:54:12 -0600

    Agrega cambios finales

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # mi_card
 
-Proyecto realizado como ejercicio de aprendizaje del curso "The Complete 2021 Flutter Development Bootcamp with Dart".
+Proyecto hecho como ejercicio del curso "The Complete 2021 Flutter Development Bootcamp with Dart".
 
-## Getting Started
+Utiliza los siguientes componentes:
 
-This project is a starting point for a Flutter application.
+## Información breve y referencias
+- [StatelessWidget: A widget that does not require mutable state.](https://api.flutter.dev/flutter/widgets/StatelessWidget-class.html)
+- [MaterialApp: An application that uses material design.](https://api.flutter.dev/flutter/material/MaterialApp-class.html)
+- [Scaffold: Implements the basic material design visual layout structure.](https://api.flutter.dev/flutter/material/Scaffold-class.html)
+- [SafeArea: A widget that insets its child by sufficient padding to avoid intrusions by the operating system.](https://api.flutter.dev/flutter/widgets/SafeArea-class.html)
+- [Column: A widget that displays its children in a vertical array.](https://api.flutter.dev/flutter/widgets/Column-class.html)
+- [CircleAvatar: A circle that represents a user.](https://api.flutter.dev/flutter/material/CircleAvatar-class.html)
+- [Text: A run of text with a single style.](https://api.flutter.dev/flutter/widgets/Text-class.html)
+- [Card: A material design card: a panel with slightly rounded corners and an elevation shadow.](https://api.flutter.dev/flutter/material/Card-class.html)
+- [ListTile: A single fixed-height row that typically contains some text as well as a leading or trailing icon.](https://api.flutter.dev/flutter/material/ListTile-class.html)
+- [Icon: A graphical icon widget drawn with a glyph from a font described in an IconData such as material's predefined IconDatas in Icons.](https://api.flutter.dev/flutter/widgets/Icon-class.html)
+- [SizedBox: A box with a specified size.](https://api.flutter.dev/flutter/widgets/SizedBox-class.html)
+- [Divider: A thin horizontal line, with padding on either side.](https://api.flutter.dev/flutter/material/Divider-class.html)
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Para más información ver la [documentación en línea](https://flutter.dev/docs).
--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,83 @@ class MyApp extends StatelessWidget {
           // Column: A widget that displays its children in a vertical array.
           child: Column(
             children: [
-              Container(
-                child: Text("Contenedor 1"),
-                height: 100.0,
-                color: Colors.white,
+              // CircleAvatar: A circle that represents a user.
+              CircleAvatar(
+                backgroundImage: AssetImage('images/giovannids_g.jpeg'),
+                radius: 50.0,
               ),
-              // SizedBox: A box with a specified size.
+              Text(
+                'Giovanni DS',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat Alternates',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FREELANCE DEVELOPER',
+                style: TextStyle(
+                  color: Colors.blueGrey.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
               SizedBox(
+                // Divider: A thin horizontal line, with padding on either side.
+                child: Divider(
+                  color: Colors.blueGrey.shade100,
+                ),
                 height: 20.0,
+                width: 150.0,
               ),
-              Container(
-                child: Text("Contenedor 2"),
-                height: 100.0,
-                color: Colors.blue,
+              // Card: A material design card: a panel with slightly rounded corners and an elevation shadow.
+              Card(
+                child: ListTile(
+                  // Icon: A graphical icon widget drawn with a glyph from a font described in an IconData such as material's predefined IconDatas in Icons.
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'giovanni@g-io.org',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                // Card: Propiedades complementarias
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
               ),
-              Container(
-                child: Text("Contenedor 3"),
-                height: 100.0,
-                color: Colors.red,
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.link,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'g-io.org',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                // Card: Propiedades complementarias
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
               ),
             ],
             // Column. Propiedades complementarias.
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ),
-        // Scaffold: Propiedades complementarias
-        backgroundColor: Colors.teal,
+        // Scaffold. Propiedades complementarias
+        backgroundColor: Colors.blueGrey.shade900,
       ),
     );
   }
@@ -46,13 +97,21 @@ class MyApp extends StatelessWidget {
 
 /*
 NOTAS:
+- CircleAvatar, Card, ListTile, e Icon
+- Las fuentes, al igual que las imagenes deben incluirse en el archivo pubspec.yaml
 - Single-child layout widgets (Containter, SizedBox, etc.), Multi-child layout widgets (Column, Row, etc.)
 - Para que hot reload funcione, lo que cambiamos deben estar dentro de un widget stateless o stateful
 - Hot restart reinicia el estado de la App, volviendo sus valores al estado inicial
 
 REFERENCIAS:
 https://flutter.dev/docs/development/ui/widgets/layout
+https://api.flutter.dev/flutter/material/Card-class.html
+https://api.flutter.dev/flutter/material/CircleAvatar-class.html
 https://api.flutter.dev/flutter/widgets/Column-class.html
 https://api.flutter.dev/flutter/widgets/Container-class.html
+https://api.flutter.dev/flutter/material/Divider-class.html
+https://api.flutter.dev/flutter/widgets/Icon-class.html
+https://api.flutter.dev/flutter/material/Icons-class.html
+https://api.flutter.dev/flutter/material/ListTile-class.html
 https://api.flutter.dev/flutter/widgets/SizedBox-class.html
  */
